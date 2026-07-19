d7ab98e9052285d77e49839c4ea802e3192a5b1b
Dibya Ranjan Panda <dibyasonu123@gmail.com>
2026-07-19 22:57:54 +0530

    Connect frontend to deployed backend

--- Frontend-Ai-rev/src/services/api.js
+++ Frontend-Ai-rev/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://code-reviewer-backend-9idm.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
@@ -91,14 +91,12 @@ export const getReviews = () => {
 };
 
 export const downloadReviewPdf = (fileId) => {
-
   return api.get(
     `/review/${fileId}/pdf`,
     {
       responseType: "blob",
     }
   );
-
 };
 
 export default api;
